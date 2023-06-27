b31ec84d7b8f34eae9125266f47f06f913bd56b2
Fatima Askerova <f.askerova@riberry.az>
2023-06-27 05:15:33 +0400

    users rest api created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,4 +10,60 @@ const router = Router();
 
 // TODO: Implement route controllers for user
 
+// GET- /api/users
+router.get("/", function (req, res, next) {
+  const users = userService.getAllUsers();
+  res.json(users);
+});
+
+// GET- /api/users:id
+router.get("/:id", function (req, res, next) {
+  const userId = req.params.id;
+  const user = userService.getUserById(userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+// POST /api/users
+router.post("/", createUserValid, function (req, res, next) {
+  const newUser = req.body;
+  const result = userService.createUser(newUser);
+
+  if (result) {
+    res.send("Data is saved");
+  } else {
+    res.status(400).send("User isn't sent");
+  }
+});
+
+// PUT /api/users/:id
+router.put("/:id", function (req, res, next) {
+  const userId = req.params.id;
+  const updatedUser = req.body;
+  const result = userService.updateUser(updatedUser, userId);
+
+  if (result) {
+    res.send("User is updated");
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+// DELETE /api/users/:id
+router.delete("/:id", function (req, res, next) {
+  const userId = req.params.id;
+  const result = userService.deleteUser(userId);
+
+  if (result) {
+    res.send("User is deleted");
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+router.use(responseMiddleware);
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,26 @@ import { userRepository } from "../repositories/userRepository.js";
 class UserService {
   // TODO: Implement methods to work with user
 
+  getAllUsers() {
+    return userRepository.getAll();
+  }
+
+  getUserById(id) {
+    return userRepository.getById(id);
+  }
+
+  createUser(user) {
+    return userRepository.create(user);
+  }
+
+  updateUser(id, updateUser) {
+    return userRepository.update(id, updateUser);
+  }
+
+  deleteUser(id) {
+    return userRepository.delete(id);
+  }
+
   search(search) {
     const item = userRepository.getOne(search);
     if (!item) {
